fbdf5367bb0840636dcca46bebc026355581d264
Mohit Vishwakarma <mohitvishwakarma681@gmail.com>
2024-01-27 00:03:20 +0530

    Sign Up and sign-In code writing done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,11 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidate } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -10,10 +15,62 @@ const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
 
-
   const handleButtonClick = () => {
-   const message = checkValidate(email.current.value,password.current.value,name.current.value);
+
+    const message = isSignInForm?checkValidate(
+      email.current.value,
+      password.current.value,
+    ):checkValidate(
+      email.current.value,
+      password.current.value,
+      name.current.value
+    );
+
     setErrorMessage(message);
+
+    if (message) return;
+
+    /** Sign In / Sign Up User **/
+    if (!isSignInForm) {
+      // Sign up logic
+
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      // Sign In logic
+
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode+" "+errorMessage);
+          console.log(errorCode, errorMessage);
+        });
+    }
   };
 
   return (
@@ -29,14 +86,13 @@ const Login = () => {
           onSubmit={(e) => e.preventDefault()}
           className="absolute bg-black w-3/12 p-12 mx-auto right-0 left-0 top-20 rounded bg-opacity-80"
         >
-
           <h2 className="text-white font-bold text-3xl mb-7">
             {isSignInForm ? "Sing In" : "Sing Up"}
           </h2>
 
           {!isSignInForm && (
             <input
-            ref={name}
+              ref={name}
               text="text"
               placeholder="Full Name"
               className="p-2 m-2  w-full bg-gray-800 text-gray-300 rounded"
